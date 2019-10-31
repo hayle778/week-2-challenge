69707ccecd8a2b7e66efc2e9bc57bd18e81804bf
GBTC407012UR <GBTC407012UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-31 16:30:38 -0400

    week-2-challenge

---
+++ src/Chall.java
@@ -0,0 +1,55 @@
+import java.util.Random;
+/*
+.write a program to assemble 10 different  burritos(input)
+. use a random array to list each burritos
+.prompt the user
+               .rice:white,brow ,no rice
+               .meat:chicken,steak, carnidas,chorizo,sofritas,veggies
+               .Beans:pinto,black,nobeans
+               . Salsa:mild,medimum,hot,nosalsa
+               . Veggies:lettuce,fajita veggies,no veggies
+.prompt the user to add list the type of Burritos choice we need
+.the out put has to list the preference
+.add the integer value to the string
+.print out all the list of the string files orderly
+.match up with the one that you print out
+.use the random array to find out the list what the user need
+
+*/
+
+
+    public class Chall {
+        public static void main(String[] args) {
+
+            String Stringfile[] = {"Rice", "Meat", "Beans", "Salsa", "Veggies", "Rice2", "Meat2", "Beans2", "Salsa2", "Veggies2"};
+            Random r = new Random(0);
+            System.out.println("Burrito" + Stringfile[7]);
+
+            System.out.println("Burrito1.Rice, meat, beans, Salsa ");
+            System.out.println("Burrito2.chicken, steak, carnidas, chorizo, sofritas, veggies");
+            System.out.println("Burrito3.pinto, black, nobeans");
+            System.out.println("Burrito4.mild, medimum, hot, nosalsa");
+            System.out.println("Burrito5.lettuce, fajita, veggies, noveggies");
+            System.out.println("Burrito6.Rice, meat, beans, Salsa, Veggies");
+            System.out.println("Burrito7.chicken, steak, carnidas, chorizo, sofritas, veggies");
+            System.out.println("Burrito8.pinto, black, nobeans");
+            System.out.println("Burrito9.mild, medimum, hot, nosalsa");
+            System.out.println("Burrito10.lettuce, fajita, veggies, noveggies");
+
+            String file = null;
+            for (String num : Stringfile) {
+
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }
